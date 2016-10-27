d9b51b0b871fba4a28830afbeedc68b5341114b2
vagrant <aileentobrien@gmail.com>
2016-10-27 02:00:22 +0000

    code refactor to seperate input and output functions

--- lookup_people.js
+++ lookup_people.js
@@ -1,20 +1,34 @@
-const pg = require('pg');
-const argv = process.argv[2]
+const pg = require("pg");
+const settings = require("./settings"); // settings.json
+const nameInput = process.argv[2];
+
+const client = new pg.Client({
+  user     : settings.user,
+  password : settings.password,
+  database : settings.database,
+  host     : settings.hostname,
+  port     : settings.port,
+  ssl      : settings.ssl
+});
+
 
 client.connect((err) => {
   if (err) {
-    return console.error("Connection Error", err);
-  } console.log('Searching...')
-  client.query("SELECT * FROM famous_people WHERE last_name =$1 OR first_name =$1;", [lastName], (err, result) => {
+    return console.log("Connection Error", err);
+  }
+    findPerson(nameInput);
+});
+
+function findPerson (nameInput) {
+  client.query("SELECT * FROM famous_people WHERE last_name =$1 OR first_name =$1;", [nameInput], (err, result) => {
     if (err) throw err;
-      console.log(`Found someone! \n`)
+      console.log(`Searching... \n Found someone!`)
       if (result.rowCount) {
         result = result.rows[0];
         console.log(`${result.id}: ${result.first_name} ${result.last_name}, born ${result.birthdate.toDateString()}`)
       }else{
         console.log(`I lied. Found no one`)
       }
-
     client.end();
   });
-})
+};
